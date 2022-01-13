2c48e4747c5bc5beb33eea98901cae77cfb3b32b
Brian Metallaoui <bmetallaoui@gmail.com>
2022-01-13 14:58:56 -0600

    separating functions

--- lib/utils/string_functions.dart
+++ lib/utils/string_functions.dart
@@ -1,4 +1,8 @@
+String capitalize(String input) {
+  return '${input[0].toUpperCase()}${input.substring(1)}';
+}
+
 String propertyToName(String input) {
-  return '${input[0].toUpperCase()}${input.substring(1)}'
+  return capitalize(input)
       .splitMapJoin(RegExp(r'([A-Z])'), onMatch: (m) => ' ${m[0]}');
 }
